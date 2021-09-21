9d552838d70268f880c4fc027a76a2e0832454df
Matt Roberts <matt.roberts@getstream.io>
2021-09-21 14:52:44 -0600

    start of new stream chat js playground

---
+++ client/chatClient.js
@@ -0,0 +1,7 @@
+require('dotenv').config({path: '/.env'});
+
+import { StreamChat } from "stream-chat";
+
+
+
+const chatClient = StreamChat.getInstance();
---
+++ index.js
@@ -0,0 +1,4 @@
+require('dotenv').config({path: '/.env'});
+
+const api_key = process.env.API_KEY;
+const secret = process.env.API_SECRET;
